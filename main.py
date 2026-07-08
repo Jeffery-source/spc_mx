d605e5b229da17bbf9e88d5aefeece0cb54de870
Jeffery <13867802383@163.com>
2026-07-08 13:15:48 +0800

    update requirements.txt

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from app.watcherdog import start_watcher
 
 if __name__ == "__main__":
 
-    folder = r"E:\\SPC"
+    folder = r"\\10.101.18.251\quality_assurance_department\CMM"
 
     start_watcher(folder)
